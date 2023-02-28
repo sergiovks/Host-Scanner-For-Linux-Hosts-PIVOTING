a0fb0b0f04506f297b0d8eb3558547e4b5893d9f
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-02-28 13:16:45 +0100

    Update host-scanner.sh

--- host-scanner.sh
+++ host-scanner.sh
@@ -11,8 +11,10 @@ trap ctrl_c INT
 
 tput civis
 
+read -p "Enter the first three octets of the IP address (e.g., 10.10.0): " ip
+
 for i in $(seq 1 254); do
-        timeout 1 bash -c "ping -c 1 10.10.0.$i" &> /dev/null && echo "[+] The host 10.10.0.$i is UP" &
+        timeout 1 bash -c "ping -c 1 $ip.$i" &> /dev/null && echo "[+] The host $ip.$i is UP" &
 done; wait
 
 tput cnorm
